29e4ee9ca3dc82a0cabe46fcc62845074428774d
dmytro_dk <dk@brsw.io>
2018-01-30 09:22:33 +0200

    Update db config

--- db/config.js
+++ db/config.js
@@ -1,7 +1,15 @@
-module.exports = {
-  development: {
-    dialect: 'sqlite',
-    storage: 'db/dev.sqlite',
-    migrationStorageTableName: '_migrations'
-  }
+const common = {
+  dialect: 'sqlite',
+  migrationStorageTableName: '_migrations',
+  operatorsAliases: false
 };
+
+const development = Object.assign({}, common, {
+  storage: 'db/dev.sqlite'
+});
+
+const production = Object.assign({}, common, {
+  storage: 'db/db.sqlite'
+});
+
+module.exports = { development, production };
