c2116d9d0b77cebf222c144cf32a447b2dc3d76c
taras-d <taras.datsenko@gmail.com>
2018-02-07 14:41:12 +0200

    Remove useless files from builds

--- electron/package.js
+++ electron/package.js
@@ -1,42 +1,54 @@
 const packager = require('electron-packager'),
-    path = require('path');
+    path = require('path'),
+    fse = require('fs-extra');
 
 const workDir = process.cwd();
+
+const ignore = [
+    'db/migrations',
+    'e2e',
+    'electron/package.js',
+    'src',
+    '.angular-cli.json',
+    '.editorconfig',
+    '.gitignore',
+    '.sequelizerc',
+    'karma.conf.js',
+    'package-lock.json',
+    'protractor.conf.js',
+    'README.md',
+    'tsconfig.json',
+    'tslint.json'
+];
 
 const options = {
     dir: workDir,
     out: path.join(workDir, 'build'),
-    overwrite: true,
-    // TODO: fix igonre list
-    // ignore: [
-    //     '/db/migrations',
-    //     '/e2e',
-    //     '/electron/package.js',
-    //     '/src',
-    //     '/\\.angular-cli\\.json',
-    //     '/\\.editorconfig',
-    //     '/\\.gitignore',
-    //     '/\\.sequelizerc',
-    //     '/karma\\.conf\\.js',
-    //     '/protractor\\.conf\\.js',
-    //     '/README\\.md',
-    //     '/tsconfig\\.json',
-    //     '/tslint\\.json'
-    // ]
+    overwrite: true
 };
 
-const clearDB = async appPaths => {
-    for (let i = 0; i < appPaths.length; ++i) {
-        const db = require( path.join(appPaths[i], 'resources/app/db/models') );
-        await db.words.destroy({ truncate: true, logging: false });
-    }
+const removeUselessFiles = appPath => {
+    return Promise.all(
+        ignore.map(item => {
+            return fse.remove( path.join(appPath, 'resources/app', item) );
+        })
+    );
+};
+
+const clearDB = appPath => {
+    const db = require( path.join(appPath, 'resources/app/db/models') );
+    return db.words.destroy({ truncate: true, logging: false });
 };
 
 packager(options, async (err, appPaths) => {
     if (err) {
         console.log(err);
     } else {
-        await clearDB(appPaths);
+        for (let i = 0; i < appPaths.length; ++i) {
+            await removeUselessFiles(appPaths[i]);
+            await clearDB(appPaths[i]);
+        }
         console.log('Packaging completed');
+        console.log(appPaths);
     }
 });
