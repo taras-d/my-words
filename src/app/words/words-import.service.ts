07b445ac79ac31baeb11797736cec119f9e30a34
taras-d <taras.datsenko@gmail.com>
2018-04-06 10:39:16 +0300

    Format ts and html

--- src/app/words/words-import.service.ts
+++ src/app/words/words-import.service.ts
@@ -10,146 +10,146 @@ import { DBService, DBConnection } from '../core/db.service';
 import { Word } from './words.service';
 
 const { ipcRenderer, remote } = nodeRequire('electron'),
-    fs = nodeRequire('fs');
+  fs = nodeRequire('fs');
 
 @Injectable()
 export class WordsImportService {
 
-    events = new Subject<string>();
+  events = new Subject<string>();
 
-    private db: DBConnection;
+  private db: DBConnection;
 
-    constructor(
-        private zone: NgZone,
-        private dbService: DBService,
-        private messagesService: MessageService
-    ) {
-        this.db = this.dbService.getConnection();
-        this.listenMenuEvents();
+  constructor(
+    private zone: NgZone,
+    private dbService: DBService,
+    private messagesService: MessageService
+  ) {
+    this.db = this.dbService.getConnection();
+    this.listenMenuEvents();
+  }
+
+  bulkCreate(words: Word[]): Observable<{ imported: number, duplicates: number }> {
+    if (!words.length) {
+      return Observable.of({ imported: 0, duplicates: 0 });
     }
 
-    bulkCreate(words: Word[]): Observable<{ imported: number, duplicates: number }> {
-        if (!words.length) {
-            return Observable.of({ imported: 0, duplicates: 0 });
+    // Exclude words with same id or text
+    const uniqueWords = [];
+    words.forEach(word => {
+      if (!word.text) {
+        return;
+      }
+
+      word = trimValues(word, 'text', 'description') as Word;
+
+      const found = uniqueWords.find(unique => {
+        return (
+          (word.id && word.id === unique.id) ||
+          (word.text.toLowerCase() === unique.text.toLowerCase())
+        );
+      });
+
+      if (!found) {
+        uniqueWords.push(word);
+      }
+    });
+
+    const db = this.db;
+    let imported = 0;
+
+    // Create word if not exist
+    const createWord = word => {
+      const where: any = {
+        [db.Sequelize.Op.or]: {
+          text: db.Sequelize.literal(
+            `text = '${DBService.escape(word.text)}' ${DBService.collateClause('NOCASE')}`
+          )
         }
+      };
 
-        // Exclude words with same id or text
-        const uniqueWords = [];
-        words.forEach(word => {
-            if (!word.text) {
-                return;
-            }
+      if (word.id) {
+        where[db.Sequelize.Op.or].id = word.id;
+      }
 
-            word = trimValues(word, 'text', 'description') as Word;
+      return this.dbService.toObservable(
+        db.words.findOne({ attributes: ['id'], raw: true, where }).then(res => {
+          return res ? null : db.words.create(word).then(() => imported++);
+        })
+      );
+    };
 
-            const found = uniqueWords.find(unique => {
-                return (
-                    (word.id && word.id === unique.id) ||
-                    (word.text.toLowerCase() === unique.text.toLowerCase())
-                );
-            });
+    // Defer requests to make sure that all words created sequentially
+    const obs = uniqueWords.map(word => Observable.defer(() => createWord(word)));
 
-            if (!found) {
-                uniqueWords.push(word);
-            }
+    // Concatenate requests and wait when last completed
+    return Observable.concat(...obs).last().map(() => {
+      return { imported, duplicates: words.length - imported };
+    });
+  }
+
+  private listenMenuEvents(): void {
+    const filters = [{ name: 'JSON', extensions: ['json'] }];
+
+    ipcRenderer.on('menu-import', () => {
+      const paths = remote.dialog.showOpenDialog({ filters });
+      if (paths) {
+        this.importWords(paths[0]);
+      }
+    });
+
+    ipcRenderer.on('menu-export', () => {
+      const path = remote.dialog.showSaveDialog({
+        filters, defaultPath: `words-${new Date().getTime()}.json`
+      });
+      if (path) {
+        this.exportWords(path);
+      }
+    });
+  }
+
+  private importWords(path: string): void {
+    this.zone.run(() => this.events.next('import'));
+
+    const readFile = Observable.bindNodeCallback(fs.readFile) as any;
+
+    readFile(path, 'utf8').mergeMap(data => {
+      try {
+        data = JSON.parse(data) || [];
+      } catch {
+        data = [];
+      }
+
+      return this.bulkCreate(data);
+    }).subscribe(result => {
+      this.zone.run(() => {
+        this.events.next('import-end');
+        this.messagesService.add({
+          severity: 'info',
+          detail: `Imported: ${result.imported}, duplicates: ${result.duplicates}`
         });
+      });
+    });
+  }
 
-        const db = this.db;
-        let imported = 0;
+  private exportWords(path: string): void {
+    this.zone.run(() => this.events.next('import'));
 
-        // Create word if not exist
-        const createWord = word => {
-            const where: any = {
-                [db.Sequelize.Op.or]: {
-                    text: db.Sequelize.literal(
-                        `text = '${DBService.escape(word.text)}' ${DBService.collateClause('NOCASE')}`
-                    )
-                }
-            };
+    const getAllWords = this.db.words.findAll({ raw: true });
 
-            if (word.id) {
-                where[db.Sequelize.Op.or].id = word.id;
-            }
-
-            return this.dbService.toObservable(
-                db.words.findOne({ attributes: ['id'], raw: true, where }).then(res => {
-                    return res ? null : db.words.create(word).then(() => imported++);
-                })
-            );
-        };
-
-        // Defer requests to make sure that all words created sequentially
-        const obs = uniqueWords.map(word => Observable.defer(() => createWord(word)));
-
-        // Concatenate requests and wait when last completed
-        return Observable.concat(...obs).last().map(() => {
-            return { imported, duplicates: words.length - imported };
+    Observable.fromPromise(getAllWords).mergeMap((words: any) => {
+      const writeFile = Observable.bindNodeCallback(fs.writeFile) as any;
+      return writeFile(
+        path, JSON.stringify(words, null, 2)
+      ).mapTo(words.length);
+    }).subscribe(count => {
+      this.zone.run(() => {
+        this.events.next('export-end');
+        this.messagesService.add({
+          severity: 'info',
+          detail: `Exported ${count} words`
         });
-    }
-
-    private listenMenuEvents(): void {
-        const filters = [{ name: 'JSON', extensions: ['json'] }];
-
-        ipcRenderer.on('menu-import', () => {
-            const paths = remote.dialog.showOpenDialog({ filters });
-            if (paths) {
-                this.importWords(paths[0]);
-            }
-        });
-
-        ipcRenderer.on('menu-export', () => {
-            const path = remote.dialog.showSaveDialog({
-                filters, defaultPath: `words-${new Date().getTime()}.json`
-            });
-            if (path) {
-                this.exportWords(path);
-            }
-        });
-    }
-
-    private importWords(path: string): void {
-        this.zone.run(() => this.events.next('import'));
-
-        const readFile = Observable.bindNodeCallback(fs.readFile) as any;
-
-        readFile(path, 'utf8').mergeMap(data => {
-            try {
-                data = JSON.parse(data) || [];
-            } catch {
-                data = [];
-            }
-
-            return this.bulkCreate(data);
-        }).subscribe(result => {
-            this.zone.run(() => {
-                this.events.next('import-end');
-                this.messagesService.add({
-                    severity: 'info',
-                    detail: `Imported: ${result.imported}, duplicates: ${result.duplicates}`
-                });
-            });
-        });
-    }
-
-    private exportWords(path: string): void {
-        this.zone.run(() => this.events.next('import'));
-
-        const getAllWords = this.db.words.findAll({ raw: true });
-
-        Observable.fromPromise(getAllWords).mergeMap((words: any) => {
-            const writeFile = Observable.bindNodeCallback(fs.writeFile) as any;
-            return writeFile(
-                path, JSON.stringify(words, null, 2)
-            ).mapTo(words.length);
-        }).subscribe(count => {
-            this.zone.run(() => {
-                this.events.next('export-end');
-                this.messagesService.add({
-                    severity: 'info',
-                    detail: `Exported ${count} words`
-                });
-            });
-        });
-    }
+      });
+    });
+  }
 
 }
--- src/app/words/words.service.ts
+++ src/app/words/words.service.ts
@@ -8,143 +8,143 @@ import { trimValues, getGoogleTranslateLink, getGoogleImagesLink } from '../core
 import { WordsImportService } from './words-import.service';
 
 export interface Word {
-    id?: string;
-    text: string;
-    translation?: string;
-    repeat?: boolean;
-    createdAt?: Date;
-    createdAtRelative?: string;
-    updatedAt?: Date;
-    updatedAtRelative?: string;
-    googleTranslateLink?: string;
-    googleImagesLink?: string;
+  id?: string;
+  text: string;
+  translation?: string;
+  repeat?: boolean;
+  createdAt?: Date;
+  createdAtRelative?: string;
+  updatedAt?: Date;
+  updatedAtRelative?: string;
+  googleTranslateLink?: string;
+  googleImagesLink?: string;
 }
 
 export interface WordsResponse {
-    skip: number;
-    limit: number;
-    total: number;
-    data: Word[];
+  skip: number;
+  limit: number;
+  total: number;
+  data: Word[];
 }
 
 
 @Injectable()
 export class WordsService {
 
-    private db: DBConnection;
+  private db: DBConnection;
 
-    constructor(
-        private dbService: DBService,
-        private wordsImportService: WordsImportService
-    ) {
-        this.db = this.dbService.getConnection();
-    }
+  constructor(
+    private dbService: DBService,
+    private wordsImportService: WordsImportService
+  ) {
+    this.db = this.dbService.getConnection();
+  }
 
-    createWords(words: Word[]): Observable<{ imported: number, duplicates: number }> {
-        return this.wordsImportService.bulkCreate(words);
-    }
+  createWords(words: Word[]): Observable<{ imported: number, duplicates: number }> {
+    return this.wordsImportService.bulkCreate(words);
+  }
 
-    getWords(
-        paging?: { skip: number, limit: number },
-        filters?: { [key: string]: any },
-        sort?: { column: string, order: number }[]
-    ): Observable<WordsResponse> {
-        const query: any = {
-            raw: true,
-            where: {},
-            order: [
-                ['createdAt', 'DESC']
-            ]
-        };
+  getWords(
+    paging?: { skip: number, limit: number },
+    filters?: { [key: string]: any },
+    sort?: { column: string, order: number }[]
+  ): Observable<WordsResponse> {
+    const query: any = {
+      raw: true,
+      where: {},
+      order: [
+        ['createdAt', 'DESC']
+      ]
+    };
 
-        paging = paging || {} as any;
-        query.offset = paging.skip || 0;
-        query.limit = paging.limit || 10;
+    paging = paging || {} as any;
+    query.offset = paging.skip || 0;
+    query.limit = paging.limit || 10;
 
-        if (filters) {
-            for (const column of Object.keys(filters)) {
-                const filter = filters[column];
-                if (column === 'repeat') {
-                    if (filter.value.val !== undefined) {
-                        query.where[column] = this.dbService.getColumnFilter({
-                            matchMode: filter.matchMode,
-                            value: filter.value.val
-                        });
-                    }
-                } else {
-                    query.where[column] = this.dbService.getColumnFilter(filter);
-                }
-            }
-        }
-
-        return this.toObs( this.db.words.findAndCountAll(query) ).map((res: any) => {
-            const words = res.rows as Word[];
-
-            words.forEach(word => {
-                word.createdAtRelative = moment( new Date(word.createdAt) ).fromNow();
-                word.updatedAtRelative = moment( new Date(word.updatedAt) ).fromNow();
-                word.googleTranslateLink = getGoogleTranslateLink(word.text);
-                word.googleImagesLink = getGoogleImagesLink(word.text);
+    if (filters) {
+      for (const column of Object.keys(filters)) {
+        const filter = filters[column];
+        if (column === 'repeat') {
+          if (filter.value.val !== undefined) {
+            query.where[column] = this.dbService.getColumnFilter({
+              matchMode: filter.matchMode,
+              value: filter.value.val
             });
-
-            return {
-                skip: query.offset,
-                limit: query.limit,
-                total: res.count,
-                data: words
-            };
-        });
+          }
+        } else {
+          query.where[column] = this.dbService.getColumnFilter(filter);
+        }
+      }
     }
 
-    getRandomWord(): Observable<Word> {
-        const sql = 'SELECT * FROM words ORDER BY RANDOM() LIMIT 1';
-        return this.toObs(this.db.sequelize.query(sql)).map((response: any) => { 
-            const word = response[0][0] || null
-            word.googleTranslateLink = getGoogleTranslateLink(word.text);
-            word.googleImagesLink = getGoogleImagesLink(word.text);
-            return word;
-        });
-    }
+    return this.toObs(this.db.words.findAndCountAll(query)).map((res: any) => {
+      const words = res.rows as Word[];
 
-    updateWord(id: string, word: Word): Observable<null> {
-        word = trimValues(word, 'text', 'translation') as Word;
+      words.forEach(word => {
+        word.createdAtRelative = moment(new Date(word.createdAt)).fromNow();
+        word.updatedAtRelative = moment(new Date(word.updatedAt)).fromNow();
+        word.googleTranslateLink = getGoogleTranslateLink(word.text);
+        word.googleImagesLink = getGoogleImagesLink(word.text);
+      });
 
-        const db = this.db;
+      return {
+        skip: query.offset,
+        limit: query.limit,
+        total: res.count,
+        data: words
+      };
+    });
+  }
 
-        const exist = db.words.findOne({
-            attributes: ['id'],
-            raw: true,
-            where: {
-                text: db.Sequelize.literal(
-                    `text = '${DBService.escape(word.text)}' ${DBService.collateClause('NOCASE')}`
-                ),
-                id: { [db.Sequelize.Op.ne]: id }
-            }
-        });
+  getRandomWord(): Observable<Word> {
+    const sql = 'SELECT * FROM words ORDER BY RANDOM() LIMIT 1';
+    return this.toObs(this.db.sequelize.query(sql)).map((response: any) => {
+      const word = response[0][0] || null
+      word.googleTranslateLink = getGoogleTranslateLink(word.text);
+      word.googleImagesLink = getGoogleImagesLink(word.text);
+      return word;
+    });
+  }
 
-        return this.toObs(exist).mergeMap(existWord => {
-            if (existWord) {
-                return this.throwWordExistError(word);
-            } else {
-                return this.toObs(
-                    db.words.update(word, { where: { id } })
-                ).mapTo(null);
-            }
-        });
-    }
+  updateWord(id: string, word: Word): Observable<null> {
+    word = trimValues(word, 'text', 'translation') as Word;
 
-    deleteWord(id: string): Observable<null> {
+    const db = this.db;
+
+    const exist = db.words.findOne({
+      attributes: ['id'],
+      raw: true,
+      where: {
+        text: db.Sequelize.literal(
+          `text = '${DBService.escape(word.text)}' ${DBService.collateClause('NOCASE')}`
+        ),
+        id: { [db.Sequelize.Op.ne]: id }
+      }
+    });
+
+    return this.toObs(exist).mergeMap(existWord => {
+      if (existWord) {
+        return this.throwWordExistError(word);
+      } else {
         return this.toObs(
-            this.db.words.destroy({ where: { id } })
+          db.words.update(word, { where: { id } })
         ).mapTo(null);
-    }
+      }
+    });
+  }
 
-    private toObs(promise: Promise<any>): Observable<any> {
-        return this.dbService.toObservable(promise);
-    }
+  deleteWord(id: string): Observable<null> {
+    return this.toObs(
+      this.db.words.destroy({ where: { id } })
+    ).mapTo(null);
+  }
 
-    private throwWordExistError(word: Word): Observable<Error> {
-        return Observable.throw(new Error(`Word "${word.text}" already exist`));
-    }
+  private toObs(promise: Promise<any>): Observable<any> {
+    return this.dbService.toObservable(promise);
+  }
+
+  private throwWordExistError(word: Word): Observable<Error> {
+    return Observable.throw(new Error(`Word "${word.text}" already exist`));
+  }
 
 }
