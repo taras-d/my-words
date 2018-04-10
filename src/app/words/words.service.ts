43bd042e1db2f86a3f42cd3bc101c34aa154cc87
taras-d <taras.datsenko@gmail.com>
2018-04-10 21:59:21 +0300

    Fix random word if no words

--- src/app/words/words.service.ts
+++ src/app/words/words.service.ts
@@ -98,10 +98,15 @@ export class WordsService {
 
   getRandomWord(): Observable<Word> {
     const sql = 'SELECT * FROM words ORDER BY RANDOM() LIMIT 1';
+    
     return this.toObs(this.db.sequelize.query(sql)).map((response: any) => {
       const word = response[0][0] || null;
-      word.googleTranslateLink = getGoogleTranslateLink(word.text);
-      word.googleImagesLink = getGoogleImagesLink(word.text);
+
+      if (word) {
+        word.googleTranslateLink = getGoogleTranslateLink(word.text);
+        word.googleImagesLink = getGoogleImagesLink(word.text);
+      }
+
       return word;
     });
   }
